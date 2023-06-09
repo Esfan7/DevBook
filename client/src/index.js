fefe642ab3fb0fda1b271b7a7192deab6e0b81b9
cristino4 <117871733+cristino4@users.noreply.github.com>
2023-06-08 19:20:29 -0700

    reorganized index.js and app.js on client, footer and navbar display

--- client/src/index.js
+++ client/src/index.js
@@ -1,59 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Link} from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import './index.css';
 import './normalize.css'
 import App from './App';
-import ProfilePage from './components/pages/ProfilePage';
-import ProjectPage from './components/pages/ProjectPage';
-import testProjects from './testData';
-import DonationSuccessPage from './components/pages/DonationSuccessPage'
-
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
 
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
- 
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Placeholder for homepage <Link to="/profile">Profile Page</Link></div>,
-  },
-  {
-      path: "/profile",
-      element: <ProfilePage/>,
-    },
-    {
-      path: "/project/:projectId",
-      element: <ProjectPage projects={testProjects} />
-    },
-    {
-      path: "/donation/success?:dollarAmount&:projectTitle",
-      element: <DonationSuccessPage />
-    },
-    {
-      path: "/donation/success",
-      element: <DonationSuccessPage />
-    }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ApolloProvider client={client}>
-    <RouterProvider router={router}>
-    </RouterProvider>
-  </ApolloProvider>,
-);
+root.render(<App />)
+
 
 
 
